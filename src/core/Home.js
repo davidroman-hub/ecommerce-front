1da5cf4e22fe7a55f4e02b7158e9d1b6da4846eb
davidroman-hub <jobroman83@gmail.com>
2020-01-28 16:31:13 +0100

    creating layout component and using in home

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import Layout from './Layout'
 
-const Home = () => <div> Home</div>
+const Home = () => (
+    <Layout title='Home Page' description="Node React e-Commerce app">
+        ...
+    </Layout>
+)
 
 export default Home
