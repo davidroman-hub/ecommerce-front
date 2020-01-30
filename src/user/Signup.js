6bc17955dfa53bd64fbd5fc5cdebc27fe0f34f0d
davidroman-hub <jobroman83@gmail.com>
2020-01-30 15:12:22 +0100

    Methods for connect the front with the API ecommerce

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -13,12 +13,51 @@ const Signup = () => {
         success:false
     });
 
+//destructarin the values with another function:
+
+const {name, email, password} = values
+
+
 // funtion will return another function for take the state
  
 
     const handleChange = name => event => {
         //we have to take all the values thats why i used " ... "
         setValues({...values, error:false, [name]:event.target.value});
+    }
+    //to create a new user we need to use the values thats why we put the method signup
+    
+    //const signup = (name, email, password) => {
+    //we change for user we had the same a the top    
+        const signup = user => {    
+    
+       // console.log(name, email, password)  this is only for check what we are sending
+       
+       //for send to the backn end we have to use the argument Fetch
+        //below is the method
+
+       fetch(`${API}/signup`,{
+           method: "POST",
+           headers:{
+               Accept:'application/json',
+               "Content-Type":"application/json"
+           },
+           body:JSON.stringify(user) //<-- THE USER ARGUMENT WILL COME FROM THE CLICK SUBMIT AND WE CAN USE LIKE AN ARGUMENT
+        //    body:JSON.stringify(name, email, password)
+       })
+       .then( response => {
+           return response.json()
+       })
+        .catch(err =>{
+            console.log(err);
+        });
+    };
+    //we have to grab the event thats why i put event
+    //we dont need to the browser will reloaded thats why we use preventDefault
+    
+    const clickSubmit = (event) => { 
+        event.preventDefault();
+        signup({name, email, password})//<-- WHE CHANGED FOR AND OBJECT {} FOR USE IN USER
     }
 
 
@@ -41,7 +80,7 @@ const Signup = () => {
                 <label className='text-muted'>Password</label>
                 <input onChange={ handleChange('password') }  type='password' className='form-control'/>
             </div>
-                <button className = "btn btn-primary">Submit</button> 
+                <button onClick={clickSubmit}  className="btn btn-primary">Submit</button> 
         </form>
     ) 
 
@@ -52,7 +91,7 @@ const Signup = () => {
         className="container col-md-8 offset-md-2">    
        
             {signUpForm()}
-            {/* {JSON.stringify(values)} this is for see the state if values are worling in the handleChange */}
+            {JSON.stringify(values)} this is for see the state if values are worling in the handleChange
     </Layout> 
     )
 }
