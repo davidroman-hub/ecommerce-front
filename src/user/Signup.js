695998b73607b3252f00e6908dc93882080c0b2c
davidroman-hub <jobroman83@gmail.com>
2020-01-30 14:05:40 +0100

    creating form of sign up

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import Layout from '../core/Layout'
+import {API} from '../config'
+const Signup = () => {
 
-const Signup = () =>(
-    (
-    <Layout title='Sign up Page' description="Sign up to Node React e-Commerce app">
-            ...
-    </Layout>
+    const signUpForm = () => (
+       
+       <form>
+
+            <div className='form-group'>
+                <label className='text-muted'>Name</label>
+                <input type='text' className='form-control'/>
+            </div>
+        
+            <div className='form-group'>
+                <label className='text-muted'>Email</label>
+                <input type='email' className='form-control'/>
+            </div>
+
+            <div className='form-group'>
+                <label className='text-muted'>Password</label>
+                <input type='password' className='form-control'/>
+            </div>
+                <button className = "btn btn-primary">Submit</button> 
+        </form>
+    ) 
+
+
+    return(
+        <Layout title='Sign up Page' 
+        description="Sign un to Node React e-Commerce app"
+        className="container col-md-8 offset-md-2">    
+            {signUpForm()}
+    </Layout> 
     )
-)
+}
 export default Signup
