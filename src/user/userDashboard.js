b300fa8d6396b8860562107944cbbe50f74d1f6c
davidroman-hub <jobroman83@gmail.com>
2020-02-04 18:50:30 +0100

    links functional on user dashbord

--- src/user/userDashboard.js
+++ src/user/userDashboard.js
@@ -1,36 +1,75 @@
 import React from 'react'
 import Layout from '../core/Layout'
 import {isAuth} from '../auth/index'
+import {Link} from 'react-router-dom'
 
 const Dashboard = () => {
 
     const {user:{_id, name, email, role}} = isAuth()
 
-    return (
-        <Layout title='Dashboard' description=' User Dashboard' className='container' >
-            <div className=' card mb-5'>
-                <h3 className='card-header'>User information</h3>
+    const userLinks = () => { 
+        return (
+            <div className='card'>
+                <h4 className='card-header'>User links</h4>
                 <ul className='list-group'>
                     <li className='list-group-item'>
-                        {name}
+                        <Link className='nav-link' to='/cart'>My Cart</Link>
                     </li>
                     <li className='list-group-item'>
-                        {email}
+                        <Link className='nav-link' to='/profile/update'>Update Profile</Link>
                     </li>
-                    <li className='list-group-item'>
-                        {role === 1 ? 'admin' : 'Registered User'} 
-                    </li>
-                </ul>
+                </ul>    
             </div>
+        )
+    }
 
+    const userInfo = () => {
+      return(
+            <div className=' card mb-5'>
+            <h3 className='card-header'>User information</h3>
+            <ul className='list-group'>
+                <li className='list-group-item'>
+                    {name}
+                </li>
+                <li className='list-group-item'>
+                    {email}
+                </li>
+                <li className='list-group-item'>
+                    {role === 1 ? 'admin' : 'Registered User'} 
+                </li>
+            </ul>
+        </div>
+        )
+    } 
+
+    const purchaseHistory = () => { 
+        return(
             <div className='card mb-5'>
-                  <h3 className='card-header'>Purchase history</h3> 
-                  <ul className='list-group'>
-                    <li className='list-group-item'>
-                        history
-                    </li>
-                 </ul>
+            <h3 className='card-header'>Purchase history</h3> 
+            <ul className='list-group'>
+              <li className='list-group-item'>
+                  history
+              </li>
+           </ul>
+      </div>
+        )
+    }
+
+    return (
+        <Layout title='Dashboard' description={`G' Day ${name}!`} className='container-fluid' >
+        
+        <div className='row'>
+            {/* <div className ='col-9'>
+                {userLinks()}
+            </div> */}
+            <div className='col-9'>
+                {userLinks()}<br/>
+                {userInfo()}
+                {purchaseHistory()}
             </div>
+        </div>
+
+           
         </Layout>
     )
 }
