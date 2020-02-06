f6f1667b673dd3e022728fdc1c6bca472fb32c44
davidroman-hub <jobroman83@gmail.com>
2020-02-06 17:43:07 +0100

    create product part 2

--- src/admin/AddProduct.js
+++ src/admin/AddProduct.js
@@ -6,11 +6,147 @@ import { createProduct } from './apiAdmin'
 
 const AddProduct = () => {
 
-
     const { user,token} = isAuth()
+    
+    //State
+
+    const [values, setValues] = useState({  //<-- have to be an object because we gonna send props
+        name:'',
+        description:'',
+        price:'',
+        categories:[], //<-- we have to put the categories from the backend
+        category:'',
+        shipping:'',
+        quantity:'',
+        photo:'',
+        loading: false,
+        error:'',
+        createdProduct:'',
+        redirectToProfile:false,
+        formData:''
+
+    })
+
+    //Destructure
+
+    const {
+
+        name,
+        description,
+        price,
+        categories, //<-- we have to put the categories from the backend
+        category,
+        shipping,
+        quantity,
+        photo,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData // <-- we gonna send this to the back end 
+
+    } = values;
+
+useEffect(() => { 
+    setValues({...values, formData: new FormData()}); //<-- thats why we used here
+}, [])
+
+
+// handleChange and clickSubmit
+
+    const handleChange = name => event => {
+        const value = name === 'photo' ? event.target.files[0] : event.target.value;
+        formData.set(name,value); //<-- faster name we gonna take the other values
+        setValues({...values,[name]: value});
+    } 
+
+    const clickSubmit = (event) => {
+        //
+    }
 
 
 
+
+    const newPostForm = () => (
+        <form className='mb-3' onSubmit={clickSubmit}>
+        
+            <h4>Post Photo</h4>
+        
+            <div className='form-group'>
+                <label className='btn btn-secondary'>
+                    <input 
+                        onChange={handleChange('photo')} 
+                        type='file' 
+                        name='photo' 
+                        accept='image/*'/>
+                </label>
+            </div>
+
+
+            <div className='form-group'>
+                <label className='text-muted'> Name </label>
+                    <input 
+                        onChange={handleChange('name')} 
+                        type='text' 
+                        className='form-control' 
+                        value={name} />
+            </div>
+
+
+            <div className='form-group'>
+                <label className='text-muted'> Description </label>
+                    <textarea 
+                        onChange={handleChange('description')}  
+                        className='form-control' 
+                        value={description} />
+            </div>
+
+            <div className='form-group'>
+                <label className='text-muted'> Price </label>
+                    <input 
+                        onChange={handleChange('price')} 
+                        type='number' 
+                        className='form-control' 
+                        value={price} />
+            </div>
+
+            <div className='form-group'>
+                <label className='text-muted'> Category </label>
+                    <select 
+                        onChange={handleChange('category')} 
+                        className='form-control' 
+                        >
+                         <option value='5e3c30a1d2a9ff2b3cd22f8e'>Node</option>   
+                    </select>
+            </div>
+            
+
+            <div className='form-group'>
+                <label className='text-muted'> Shipping </label>
+                    <select 
+                        onChange={handleChange('shipping')} 
+                        className='form-control' 
+                        >
+                         <option value='0'>No</option>
+                         <option value='1'>yes</option>      
+                    </select>
+            </div>
+
+
+            <div className='form-group'>
+                <label className='text-muted'> Quantity </label>
+                    <input 
+                        onChange={handleChange('quantity')} 
+                        type='number' 
+                        className='form-control' 
+                        value={quantity} />
+            </div>
+
+            <button className='btn btn-outline-primary'> Create Product </button>
+            
+
+        </form>
+    )
 
 
     return(
@@ -21,7 +157,7 @@ const AddProduct = () => {
         <div className='row'>
           
             <div className='col-md-8 offset-md-2'>
-                    ...
+                  {newPostForm()}
 
             </div>
         </div>
