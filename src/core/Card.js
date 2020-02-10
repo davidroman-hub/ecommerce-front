d3c870a61a66b216696ab6f409f5cbbf8f644d85
davidroman-hub <jobroman83@gmail.com>
2020-02-10 13:55:01 +0100

    add image to a card

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-
+import ShowImage from './ShowImage'
 
 const Card = ({product}) => {
     return (
@@ -9,7 +9,8 @@ const Card = ({product}) => {
                 <div className='card-header'>{product.name}</div>
                 <div className='card-body'>
                     <p>{product.description}</p>
-                    <p>{product.price}</p>
+                    <ShowImage item={product} url='product'/>
+                    <p>${product.price}</p>
                     <Link to='/'/>
                     <button className='btn btn-outline-primary mt-2 mb-2'>
                         View Product
