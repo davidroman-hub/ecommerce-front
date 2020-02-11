f6a1aafb718485ac6792fb0ce6b8ff1146064938
davidroman-hub <jobroman83@gmail.com>
2020-02-11 18:18:31 +0100

    get the categories in shop paGE

--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -4,16 +4,6 @@ import {API} from '../config'
 //Get products method
 
 
-// export const getProducts = (sortBy) => {
-//     return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
-//         method: 'GET'
-//     })
-//     .then( response => {
-//         return response.json()
-//     })
-//     .catch(err => console.log(err))
-// }
-
 export const getProducts = sortBy => {
     return fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
         method: "GET"
@@ -23,3 +13,17 @@ export const getProducts = sortBy => {
         })
         .catch(err => console.log(err));
 };
+
+
+//GET the categories methods
+
+
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: 'GET'
+    })
+    .then( response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
