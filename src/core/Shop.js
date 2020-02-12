05d70f448b1abef3d90fa30c79e66ac444a3aca9
davidroman-hub <jobroman83@gmail.com>
2020-02-12 15:43:42 +0100

    load more button

--- src/core/Shop.js
+++ src/core/Shop.js
@@ -12,14 +12,15 @@ const Shop = () => {
 const [myFilters, setMyFilters] = useState({
  filters:{category: [], price: []}
 })
-const [categories, setCategories] = useState([])
-const [error, setError] = useState(false)
+const [categories, setCategories] = useState([]);
+const [error, setError] = useState(false);
 
 // for show the product with the filters
 
-const [limit, setLimit] = useState(6)
-const [skip, setSkip] = useState(0)
-const [filteredResults, setFilteredResults] = useState([]) //< --- el error estaba aqui 
+const [limit, setLimit] = useState(6);
+const [skip, setSkip] = useState(0);
+const [size, setSize] = useState(0);
+const [filteredResults, setFilteredResults] = useState([]); //< --- el error estaba aqui 
 
 
 const init = () => {
@@ -38,11 +39,39 @@ const loadFilteredResults = newFilters => {
     if(data.error){
         setError(data.error)
     } else {
-        setFilteredResults(data.data)
-    }
-})
-
+        setFilteredResults(data.data);
+        // button for charge more products
+        setSize(data.size)
+        setSkip(0)
+        }
+    })
 };
+
+// method for load more products
+
+const loadMore = () => { 
+    let toSkip = skip + limit // remember the skip its 0 but the limit its 6 so, we show 6 more
+    // console.log(newFilters)
+  getFilteredProducts(toSkip, limit, myFilters.filters).then( data => {
+     if(data.error){
+         setError(data.error)
+     } else {
+         setFilteredResults([...filteredResults, ...data.data]);
+         // button for charge more products
+         setSize(data.size)
+         setSkip(toSkip)
+         }
+     })
+ };
+
+const loadMoreButton = () => {
+    return (
+        size > 0 && size >= limit && (
+            <button onClick={loadMore} className="btn btn-warning mb-5">Load more</button>
+        )
+    )
+}
+
 
 
 useEffect(() => {
@@ -123,6 +152,8 @@ const handlePrice = value => {
                             </div>
                     ))} */}
                   </div>
+                  <hr/>
+                  {loadMoreButton()}
               </div>
           </div>
            
