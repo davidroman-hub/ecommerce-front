2a64f7932a3f2bdd49a5c5e2a62aa44390793360
davidroman-hub <jobroman83@gmail.com>
2020-02-12 13:48:56 +0100

    filter with price range, to get the arrays

--- src/core/RadioBox.js
+++ src/core/RadioBox.js
@@ -1,9 +1,13 @@
 import React, {useState, useEffect, Fragment} from 'react';
 
-const RadioBox = ({prices}) => {
-    const [value, setValues] =useState(0)
-    const handleChange = () => {
-        //
+const RadioBox = ({prices,handleFilters}) => {
+    const [value, setValue] =useState(0)
+    
+    
+    
+    const handleChange = event => {
+        handleFilters(event.target.value);
+        setValue(event.target.value);
     }
 
    return prices.map ((p, i)=>(
@@ -11,6 +15,7 @@ const RadioBox = ({prices}) => {
            <input 
            onChange={handleChange}
            value={`${p._id}`}
+           name={p}
            type="radio"
            className='mr-2 ml-4'
            />
--- src/core/Shop.js
+++ src/core/Shop.js
@@ -8,7 +8,8 @@ import {prices} from './fixedPrices'
 import RadioBox from './RadioBox'
 
 const Shop = () => {
-const [myFilters, setMyFilters] = useState({
+
+    const [myFilters, setMyFilters] = useState({
     filters:{category: [], price: []}
 })
 const [categories, setCategories] = useState([])
@@ -32,11 +33,27 @@ useEffect(() => {
 
 const handleFilters = (filters,filterBy) => {
    // console.log('SHOP',filters, filterBy)
-   const newFilters = {...myFilters}
-   newFilters.filters[filterBy] = filters
-   setMyFilters(newFilters)
-}
+   const newFilters = {...myFilters};
+   newFilters.filters[filterBy] = filters;
+   
+   if(filterBy == 'price') {
+        let priceValues = handlePrice(filters)
+        newFilters.filters[filterBy] = priceValues;
+    }
+   setMyFilters(newFilters);
+};
 
+const handlePrice = value => {
+    const data = prices;
+    let array = [];
+    
+    for (let key in data) {
+        if(data[key]._id === parseInt(value)) {
+            array = data[key].array
+        }
+    }
+    return array;
+}
 
 
 
