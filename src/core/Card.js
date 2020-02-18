89da6fab27c7c005520722041ce926645e96b83d
davidroman-hub <jobroman83@gmail.com>
2020-02-18 13:31:11 +0100

    product detail on single page product part 1

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import ShowImage from './ShowImage'
+import moment from 'moment'
+
 
 const Card = ({ product, showViewProductButton = true }) => {
 
@@ -20,10 +22,18 @@ const Card = ({ product, showViewProductButton = true }) => {
         // <div className='col-4 mb-3'>
             <div className='card'>
                 <div className='card-header'>{product.name}</div>
-                <div className='card-body'>
-                    <p>{product.description.substring(0,100)}</p>
+                <div className='card-body'>      
                     <ShowImage item={product} url='product'/>
-                    <p>${product.price}</p>
+                    <p className=' lead mt-2'>
+                        {product.description.substring(0,100)}
+                        </p>
+                    <p className='black-9'>${product.price}</p>
+                    <p className='black-8'>Category:{product.category && product.category.name}</p>
+                    <p className='black-8' >
+                        Added on {moment(product.createdAt).fromNow()}
+                         {/* <--- very friedly for the clients! */}
+                    </p>
+                 
                     
                     {/* <button className='btn btn-outline-primary mt-2 mb-2 mr-2'>
                         View Product
