486f9a1a73913b0ca3b6078f09003fdb9c55dd0f
davidroman-hub <jobroman83@gmail.com>
2020-02-19 11:47:42 +0100

    Add to cart funtion and redirect functionality

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,10 +1,12 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React,{ useState,useEffect} from 'react'
+import {Link, Redirect} from 'react-router-dom'
 import ShowImage from './ShowImage'
 import moment from 'moment'
-
+import {addItem} from './cartHelpers'
 
 const Card = ({ product, showViewProductButton = true }) => {
+
+    const [redirect, setRedirect] = useState(false)//<State for the cart redirect
 
     const showViewButton = (showViewProductButton) => {
         return (
@@ -17,10 +19,22 @@ const Card = ({ product, showViewProductButton = true }) => {
         )
     }
 
+    //we import the funtion from cart helpers and we gonna execute
+
+const addToCart = () => {
+    addItem(product,setRedirect(true))
+}
+const shouldRedirect = redirect => {
+    if(redirect){
+        return <Redirect to='/cart'/>
+    }
+}
+
+
     const showAddToCartButton = () => {
         
         return(
-            <button className='btn btn-outline-warning mt-2 mb-2'>
+            <button  onClick={addToCart} className='btn btn-outline-warning mt-2 mb-2'>
                         Add to card
             </button>
         )
@@ -38,7 +52,8 @@ const Card = ({ product, showViewProductButton = true }) => {
         // <div className='col-4 mb-3'>
             <div className='card'>
                 <div className='card-header name'>{product.name}</div>
-                <div className='card-body'>      
+                <div className='card-body'> 
+                    {shouldRedirect(redirect)}     
                     <ShowImage item={product} url='product'/>
                     <p className=' lead mt-2'>
                         {product.description.substring(0,100)}
