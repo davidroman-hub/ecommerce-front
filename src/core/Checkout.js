c5a3f37c8b00a9279fe3bb82ef282b899dfbe1a9
davidroman-hub <jobroman83@gmail.com>
2020-02-19 15:45:13 +0100

    checkout part 2

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -1,7 +1,10 @@
 import React, {useState,useEffect} from 'react'
+import {Link} from 'react-router-dom'
 import Layout from './Layout'
 import {getProducts} from './apiCore'
 import Card from './Card'
+import  {isAuth} from '../auth/index'
+
 
 const Checkout = ({product}) => {
 
@@ -13,10 +16,25 @@ const Checkout = ({product}) => {
     }
 
 
+const showCheckout = () => {
+ return  isAuth() ? ( <button className='btn btn-succes'>Checkout</button>
+) : (
+<Link to='/signin'>
+    <button className='btn btn-primary'>
+        Sign in to Checkout
+    </button>
+</Link>
+    )}
+
 
     return (
         // <div>{JSON.stringify(product)}</div>
+      <div> 
         <h2> Total: ${getTotal()}</h2>
+
+       {showCheckout()}
+     
+    </div>
     )
 
 }
