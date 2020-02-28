2493bd8612c03d980f15f2f1e881bfac00f99868
davidroman-hub <jobroman83@gmail.com>
2020-02-28 16:20:23 +0100

    user history purchase with all the data

--- src/user/userDashboard.js
+++ src/user/userDashboard.js
@@ -4,7 +4,7 @@ import {isAuth} from '../auth/index'
 import {Link} from 'react-router-dom'
 import Profile from './Profile'
 import {getPurchaseHistory} from './apiUser'
-
+import moment from 'moment'
 
 
 const Dashboard = () => {
@@ -67,18 +67,37 @@ const Dashboard = () => {
         )
     } 
 
-    const purchaseHistory = (history) => { 
-        return(
-            <div className='card mb-5'>
-            <h3 className='card-header'>Purchase history</h3> 
-            <ul className='list-group'>
-              <li className='list-group-item'>
-                  {JSON.stringify(history)}
-              </li>
-           </ul>
-      </div>
-        )
-    }
+
+    const purchaseHistory = history => {
+        return (
+            <div className="card mb-5">
+                <h3 className="card-header">Purchase history</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                        {history.map((h, i) => {
+                            return (
+                                <div>
+                                    <hr />
+                                    {h.products.map((p, i) => {
+                                        return (
+                                            <div key={i}>
+                                                <h6>Product name: {p.name}</h6>
+                                                <h6>Product price: ${p.price}</h6>
+                                                <h6>
+                                                    Purchased date:{" "}
+                                                    {moment(p.createdAt).fromNow()}
+                                                </h6>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            );
+                        })}
+                    </li>
+                </ul>
+            </div>
+        );
+    };
 
     return (
         <Layout title='Dashboard' description={`G' Day ${name}!`} className='container-fluid' >
