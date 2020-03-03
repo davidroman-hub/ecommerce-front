ac29a4815275d9407059788e0b3cc2459126da47
davidroman-hub <jobroman83@gmail.com>
2020-03-03 13:09:49 +0100

    products methods for update...

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -105,3 +105,69 @@ export const updateOrderStatus = (userId,token,orderId, status) => {
     })
     .catch(err => console.log(err))
 }
+
+
+/**
+ * 
+ * to perform crud on product to manage them
+ */
+
+ /// get all the products
+
+ export const getProducts= () => {
+    return fetch(`${API}/products`, {
+        method: 'GET'
+    })
+    .then( response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+ //get a single product
+ export const getProduct = (productId) => {
+    return fetch(`${API}/product/${productId}`, {
+        method: 'GET'
+    })
+    .then( response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+ // update single product
+
+ export const updateProduct = (productId,userId, token,product) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept:"application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+    .then( response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+ 
+ // delete single product
+
+ export const deleteProduct = (productId,userId, token) => {
+    return fetch(`${API}/product/${productId}/${userId}`, {
+        method: "DELETE",
+        headers: {
+            Accept:"application/json",
+            "Content-Type":"application/json",
+            Authorization: `Bearer ${token}`
+        },
+    })
+    .then( response => {
+        return response.json()
+    })
+    .catch(err => console.log(err))
+}
+
+ 
