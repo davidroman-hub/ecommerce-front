7e9fc5440f66352ffa6f6e9234e223b04406450d
davidroman-hub <jobroman83@gmail.com>
2020-03-03 14:00:20 +0100

    adding delate product

--- src/admin/ManageProducts.js
+++ src/admin/ManageProducts.js
@@ -1,10 +1,68 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import Layout from '../core/Layout'
 import {isAuth} from '../auth/index'
 import {Link} from 'react-router-dom'
+import {getProducts, deleteProduct} from './apiAdmin'
 
 
-const ManageProducts = () => { 
+
+const ManageProducts = () => {   
+
+const [products,setProducts] = useState([])
+
+const {user,token} = isAuth()
+
+///// get all the products from the back /////
+
+const loadProducts = () => {
+    getProducts().then(
+        data => {
+            if (data.error){
+                console.log(data.error)
+            } else {
+                setProducts(data)
+            }
+        }
+    )
+}
+
+//// delete product ////
+
+const destroy = productId => {
+    deleteProduct(productId, user._id, token).then(data => {
+        if(data.error){
+            console.error(data.error)
+        } else {
+            loadProducts()
+        }
+    })
+}
+
+const productsList = () => {
+    return (
+        <div className='col-12'>
+            <ul className='list-group'>
+                {products.map((p,i)=>(
+                    <li key={i} className='list-group-item justify-content-between align-items-center'>
+                        <strong>{p.name}</strong>
+                        <Link to={`/admin/product/update/${p.id}`}>
+                            <span className='badge badge-warning badge-pill'>Update</span>
+                        </Link>                   
+                        <span onClick={()=> destroy(p._id)}
+                         className='badge badge-danger badge-pill'>
+                             Delate
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </div>  
+    )
+}
+
+
+useEffect(() => {
+    loadProducts()
+},[])
 
     return (
         <Layout title="Manage Products" 
@@ -12,9 +70,7 @@ const ManageProducts = () => {
         className='container-fluid'
         >
             <div className='row'>
-                <div className=''>
-                    ....
-                </div>
+               {productsList()}
             </div>
        
        
